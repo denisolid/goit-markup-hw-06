5c4d63963c85eec23a68e39deed7cb655020d2e9
denisolid <dkolisnichienko321@gmail.com>
2024-02-22 22:20:13 +0000

    Update modal.js

--- modal.js
+++ modal.js
@@ -15,4 +15,25 @@ function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle("is-open");
 }
+
+
+
+const rems = {
+    // Додати атрибут data-menu-open на кнопку відкриття
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    // Додати атрибут data-menu-close на кнопку закриття
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    // Додати атрибут data-menu на бекдроп модалки
+    menu: document.querySelector("[data-menu]"),
+};
+
+rems.openMenuBtn.addEventListener("click", toggleMenu);
+rems.closeMenuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    rems.menu.classList.toggle("is-open");
+}
+
+
 })();
